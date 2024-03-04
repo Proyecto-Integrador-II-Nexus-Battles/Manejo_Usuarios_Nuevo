40e73b3808796c1cb882be73f6080ade3bd69a41
TiltRex <afls2002@gmail.com>
2024-03-04 13:26:46 -0500

    Update models

--- models/models.js
+++ models/models.js
@@ -2,17 +2,17 @@ import { pool } from "./db.js";
 // En esta clase se encargaran de hacer la logica de negocio, también realizar conexión a la base de datos desde este *archivo* <-- !IMPORTANT
 export class userModel {
     static async getUsers() {
-        const usernamesResult = await pool.query("SELECT nombre FROM users");
+        const usernamesResult = await pool.query("SELECT usuario FROM users");
         return usernamesResult;
     }    
 
     static async searchUsers(query) {
-        const resultados = await pool.query("SELECT nombre FROM users WHERE nombre LIKE ?", [`${query}%`]);
+        const resultados = await pool.query("SELECT usuario FROM users WHERE usuario LIKE ?", [`${query}%`]);
         return resultados;
     } 
 
     static async getUserInfo(username) {
-        const userResult = await pool.query("SELECT nombre, apellido, usuario, email, metodospago FROM users WHERE nombre = ?", [username]);
+        const userResult = await pool.query("SELECT nombre, apellido, usuario, email, metodospago FROM users WHERE usuario = ?", [username]);
         return userResult[0];
     } 
     // getByID
