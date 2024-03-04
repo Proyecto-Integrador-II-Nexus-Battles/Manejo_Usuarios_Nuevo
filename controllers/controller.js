b1d1689837682172d804ea1e3e8ae30c38011618
Sofía princess <sofusaka@gmail.com>
2024-03-03 21:45:23 -0500

    v1
    withoutregister

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,24 +1,59 @@
 import { userModel } from "../models/models.js"
+import bcrypt from 'bcrypt';
+import { json } from "express";
+import session from 'express-session';
 
 
 // En esta clase se encarga de controlar los datos y consultas (get-post) y devolverlas en formato JSON
 // Hacia la vista que las requiera 
 export class userController { // --> TODOS LOS ARCHIVOS FUNCIONAM COMO UNA CAJA FUERTE
-    static async getUsername (req, res) {
+    static async getUsername(req, res) {
         const usernames = await userModel.getUsers();
         res.json(usernames);
     }
 
-    static async buscarUsername (req, res) {
+    static async buscarUsername(req, res) {
         const query = req.query.q;
         const resultados = await userModel.searchUsers(query);
         res.json(resultados);
     }
 
-    static async getUserInfoController (req, res) {
+    static async getUserInfoController(req, res) {
         const username = req.params.username;
         const user = await userModel.getUserInfo(username);
         res.json(user);
+    }
+
+    static async LogIn(req, res) {
+
+        let email = req.body.email;
+        let password = req.body.password;
+
+        let email_db = await userModel.getUserEmail(email);
+        let password_db = await userModel.getPassword(email);
+
+        if (email === email_db && bcrypt.compareSync(password, password_db)) {
+
+            if (req.session.authenticated) {
+
+                res.json(req.session.user);
+
+            } else {
+
+                req.session.authenticated = true;
+                req.session.user = {
+                    email, id
+                };
+                console.log(req.session.user)
+                res.json(req.session.user);
+            }
+
+
+        } else {
+
+            res.json('not authenticated')
+        }
+
     }
     // Create
     // Upload
--- models/models.js
+++ models/models.js
@@ -4,17 +4,39 @@ export class userModel {
     static async getUsers() {
         const usernamesResult = await pool.query("SELECT nombre FROM users");
         return usernamesResult;
-    }    
+    }
 
     static async searchUsers(query) {
         const resultados = await pool.query("SELECT nombre FROM users WHERE nombre LIKE ?", [`${query}%`]);
         return resultados;
-    } 
+    }
 
     static async getUserInfo(username) {
         const userResult = await pool.query("SELECT nombre, apellido, usuario, email, metodospago FROM users WHERE nombre = ?", [username]);
         return userResult[0];
-    } 
+    }
+
+    static async getUserEmail(email) {
+
+        let email_db = await pool.query("SELECT email FROM users WHERE email=?", [email]);
+        return email_db[0];
+
+    }
+
+    static async getPassword(email) {
+
+        let password_db = await pool.query("SELECT password FROM users WHERE email=?", [email]);
+        return password_db[0];
+    }
+
+    static async getUserID(email) {
+
+        let id = await pool.query("SELECT id FROM users WHERE email=?", [email]);
+        return id[0];
+    }
+
+
+
     // getByID
     // CREATE
     // UPDATE
