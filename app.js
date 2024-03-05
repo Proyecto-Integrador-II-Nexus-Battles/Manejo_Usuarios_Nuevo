d3fc5afcd42e2735f220058ac56ce90401cfa8b7
TiltRex <afls2002@gmail.com>
2024-03-05 11:19:24 -0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express, { json } from 'express'
 import { userRouter } from './routes/routes.js' //--> !!!IMPORTANT!!! Siempre que importen un archivo extensión .js .Loquesea, siempre ponerlo en el path, ej -> './routes/template.js' --> el .js es la extensión 
 
@@ -6,7 +7,7 @@ app.use(json())
 app.disable('x-powered-by') // --> Deshabilitar el header x-powered-by
 
 app.use(cors())
-app.use('usuario', userRouter)
+app.use('/usuario', userRouter)
 
 const PORT = process.env.PORT || 3000 // --> Usar la variable de entorno PORT, si no usar el port 3000
 
