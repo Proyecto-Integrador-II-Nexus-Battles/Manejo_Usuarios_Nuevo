730fa85559dd150dc781449b4fbda1befc13abc9
TiltRex <afls2002@gmail.com>
2024-03-05 08:50:21 -0500

    update

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const app = express() // --> Iniciamos express
 app.use(json()) 
 app.disable('x-powered-by') // --> Deshabilitar el header x-powered-by
 
-app.use(userRouter)
+app.use(cors())
+app.use('usuario', userRouter)
 
-const PORT = process.env.PORT || 5000 // --> Usar la variable de entorno PORT, si no usar el port 3000
+const PORT = process.env.PORT || 3000 // --> Usar la variable de entorno PORT, si no usar el port 3000
 
 app.listen(PORT, () => {
   console.log(`Server listen on port http://localhost:${PORT}`)
