59ef4f54943fdd025a3820816fbf56e492d03b71
Sofía princess <sofusaka@gmail.com>
2024-03-06 11:36:06 -0500

    finalmente funcional el register
    register funcional

--- app.js
+++ app.js
@@ -2,23 +2,19 @@ import express, { json } from "express";
 import { userRouter } from "./routes/routes.js"; //--> !!!IMPORTANT!!! Siempre que importen un archivo extensión .js .Loquesea, siempre ponerlo en el path, ej -> './routes/template.js' --> el .js es la extensión
 import { PORT } from "./config.js";
 import bodyParser from "body-parser";
-
+import cors from "cors";
 
 const app = express(); // --> Iniciamos express
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.disable("x-powered-by"); // --> Deshabilitar el header x-powered-by
-
-
-
 
 app.use("/usuario", userRouter); // --> Usar la variable de entorno PORT, si no usar el port 3000
 
 
 
 app.use(function (req, res, next) {
-
-  res.append('Access-Control-Allow-Origin', ['http://localhost:5000/register']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next()
