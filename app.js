e27b9eed24d8dca6a96939684fc1e34a762a7f4f
Santiadi <disacamo43@gmail.com>
2024-03-19 17:35:16 -0500

    Puertos app.js y config.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res) => {
 app.use(cors())
 app.use('/usuario', userRouter)
 
-const PORT = process.env.PORT || 3000 // --> Usar la variable de entorno PORT, si no usar el port 3000
+const PORT = process.env.PORT || 4443 // --> Usar la variable de entorno PORT, si no usar el port 3000
 
 app.listen(PORT, () => {
   console.log(`Server listen on port http://localhost:${PORT}`);
--- config.js
+++ config.js
@@ -6,5 +6,5 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 config({ path: path.resolve(__dirname, ".env") });
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 4443;
 export const JWT_SECRET = process.env.JWT_SECRET || "";
