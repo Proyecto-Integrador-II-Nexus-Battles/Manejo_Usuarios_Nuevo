411bb29eaf721b4a78d71f5c20226be02e5c047a
TiltRex <afls2002@gmail.com>
2024-03-25 16:25:04 -0500

    Variables de entorno

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express, { json } from 'express'
 import { userRouter } from './routes/routes.js' //--> !!!IMPORTANT!!! Siempre que importen un archivo extensión .js .Loquesea, siempre ponerlo en el path, ej -> './routes/template.js' --> el .js es la extensión 
+import { APP_PORT } from "./config.js";
 
 const app = express() // --> Iniciamos express
 app.use(json()) 
@@ -9,8 +10,6 @@ app.disable('x-powered-by') // --> Deshabilitar el header x-powered-by
 app.use(cors())
 app.use('/usuario', userRouter)
 
-const PORT = process.env.PORT || 3000 // --> Usar la variable de entorno PORT, si no usar el port 3000
-
-app.listen(PORT, () => {
-  console.log(`Server listen on port http://localhost:${PORT}`)
+app.listen(APP_PORT, () => {
+  console.log(`Server listen on port ${APP_PORT}`)
 })
